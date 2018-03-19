765b371928a979a333d5c7e0ed3c26548a854158
Neetshin <neetshin@neetsh.in>
2018-03-19 13:01:00 +0530

    Install dependencies

---
+++ src/scripts/containers/Neetshin.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Neetshin extends React.PureComponent {
+
+  render () {
+    return (
+      <div>
+        Neetshin DESU!!
+      </div>
+    );
+  }
+
+}
