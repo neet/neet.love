e3536cc50d6a71a16eac55f9c677adee3d71b020
neet <n33t5hin@gmail.com>
2018-05-28 17:18:34 +0900

    Add Google fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,14 @@ module.exports = {
     'gatsby-plugin-typescript',
     'gatsby-plugin-offline',
     'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: [
+          'PT Sans',
+          'Pacifico',
+        ]
+      }
+    }
   ],
 }
