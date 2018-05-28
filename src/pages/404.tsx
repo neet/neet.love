45bd8dd476476b90a01e3e26f9fbd78a3af5ad1f
neet <n33t5hin@gmail.com>
2018-05-29 00:31:28 +0900

    Apply Page for 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import Page from '../components/page';
 
 const NotFoundPage: React.SFC = () => (
-  <div>
+  <Page>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  </Page>
 );
 
 export default NotFoundPage;
