17e088a7fce75867ca2fb5249faef720b986b0eb
neet <n33t5hin@gmail.com>
2018-05-30 11:11:46 +0900

    Integrate social account component

--- src/components/social_accounts.tsx
+++ src/components/social_accounts.tsx
@@ -1,25 +1,57 @@
 import * as React from 'react';
-import SocialAccountButton from './social_account_button';
+import * as copy from 'copy-to-clipboard';
 import { SocialAccount } from '../pages/index';
 
 interface Props {
   socialAccounts: { node: SocialAccount }[];
 }
 
-const SocialAccounts: React.SFC<Props> = ({ socialAccounts }) => {
-  return (
-    <div className='social-accounts'>
-      <ul className='social-accounts__list'>
-        {
-          socialAccounts.map(({ node }, i) => (
-            <li className='social-accounts__list-item' key={`${i}-${node.name}`}>
-              <SocialAccountButton socialAccount={node} />
-            </li>
-          ))
-        }
-      </ul>
-    </div>
-  );
-};
+export default class SocialAccounts extends React.PureComponent<Props> {
 
-export default SocialAccounts;
+  private handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const i = e.currentTarget.getAttribute('data-index') as string;
+    const socialAccount = this.props.socialAccounts[Number(i)].node;
+
+    if (socialAccount.copy) {
+      e.preventDefault();
+      copy(socialAccount.copy);
+      alert(`${socialAccount.copy}をコピーしました`);
+    }
+  }
+
+  private renderItem = (socialAccount: SocialAccount, i: number) => {
+    return (
+      <li className='social-accounts__list-item' key={`${i}-${socialAccount.name}`} aria-posinset={i+1} aria-setsize={this.props.socialAccounts.length}>
+        <div className='social-account-button'>
+          <a
+            className='social-account-button__link'
+            href={socialAccount.href}
+            title={socialAccount.label}
+            data-index={i}
+            onClick={this.handleClick}
+          >
+            <span className='social-account-button__fa-wrapper'>
+              <i className={`social-account-button__fa ${socialAccount.fa}`} aria-hidden />
+            </span>
+
+            <span className='social-account-button__label'>
+              {socialAccount.name}
+            </span>
+          </a>
+        </div>
+      </li>
+    );
+  }
+
+  public render () {
+    const { socialAccounts } = this.props;
+
+    return (
+      <div className='social-accounts'>
+        <ul className='social-accounts__list'>
+          { socialAccounts.map(({ node }, i) => this.renderItem(node, i)) }
+        </ul>
+      </div>
+    );
+  }
+}
