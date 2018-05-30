b64e2ea13f5d973c822617f0118f8056d33cfda7
neet <n33t5hin@gmail.com>
2018-05-31 00:43:25 +0900

    Update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import Content from '../components/content';
 import Page from '../components/page';
 
 const NotFoundPage: React.SFC = () => (
-  <Page title='お探しのページは虚無'>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Page>
+  <Content>
+    <Page title='お探しのページは虚無'>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Page>
+  </Content>
 );
 
 export default NotFoundPage;
