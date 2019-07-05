c5860c4a9f0a187c0a2f9424bc8d5ae38ffaa25b
Neetshin <n33t5hin@gmail.com>
2019-07-05 19:04:06 +0900

    Edit configs which i dislike

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import 'modern-normalize'
 import '../styles/normalize'
 
 import Header from '../components/Header'
