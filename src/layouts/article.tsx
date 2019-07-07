c67ef5aed31b64eaf17e163bfa60ff55f132eb82
Neetshin <n33t5hin@gmail.com>
2019-07-08 02:37:20 +0900

    Restore helmet

--- src/layouts/article.tsx
+++ src/layouts/article.tsx
@@ -1,33 +1,46 @@
-// import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
-// import Helmet from 'react-helmet';
+import Helmet from 'react-helmet';
 import styled from 'styled-components';
 import { Banner } from '../components/banner';
 import { Footer } from '../components/footer';
 import { GlobalStyle } from '../styles/global-style';
 
-const Wrapper = styled.main``;
+const Wrapper = styled.div``;
 
-const Content = styled.div`
+const Content = styled.main`
   width: 700px;
   margin: auto;
   padding: 24px 0;
 `;
 
-// <Helmet
-//   title={data.site.siteMetadata.title}
-//   meta={[
-//     { name: 'description', content: data.site.siteMetadata.description },
-//     { name: 'keywords', content: data.site.siteMetadata.keywords },
-//   ]}
-// />
+interface ArticleQueryData {
+  site: {
+    siteMetadata: {
+      title: string;
+      description: string;
+    };
+  };
+}
 
 export const Article: React.SFC = ({ children }) => {
+  const data = useStaticQuery<ArticleQueryData>(graphql`
+    query ArticleQuery {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `);
+
   return (
     <Wrapper>
+      <Helmet title={data.site.siteMetadata.title} meta={[{ name: 'description', content: data.site.siteMetadata.description }]} />
       <Banner />
       <Content>{children}</Content>
-      <Footer/>
+      <Footer />
       <GlobalStyle />
     </Wrapper>
   );
