8859bee3885773feece9146e6cdd07826b42ea84
Neetshin <n33t5hin@gmail.com>
2019-07-10 05:30:46 +0900

    Update theme colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,7 +78,7 @@ module.exports = {
         name: 'neet.love',
         start_url: '/',
         background_color: '#ffffff',
-        theme_color: '#663399',
+        theme_color: '#0076FF',
         display: 'minimal-ui',
         icon: 'content/assets/avatar.jpg',
       },
